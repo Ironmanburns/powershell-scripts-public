aea6c873b313e5c0a6dc6f5edf551744c3d65bff
Jason Burns <jason_burns@outlook.com>
2019-03-15 14:47:39 +0000

    added some extra comments around the functions

--- scripts/get-authtokenfalkusway.ps1
+++ scripts/get-authtokenfalkusway.ps1
@@ -1,3 +1,5 @@
+#heres me trying to connect to azure for an identity
+
 $AadModule = Get-Module -Name "AzureAD" -ListAvailable
 $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
 $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
@@ -11,17 +13,20 @@ $authority = "https://login.microsoftonline.com/mercurypoc.onmicrosoft.com"
 $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
 $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
 $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
-#adminconsent
+
+#heres me asking for adminconsent for some extra permissions on your tenant
 #$authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId,"prompt=admin_consent").Result
 $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
 $authresult |Get-Member
 
+#heres me creating the authheader for talking to graph
 $authHeader = @{
     'Content-Type'='application/json'
     'Authorization'="Bearer " + $authResult.AccessToken
     'ExpiresOn'=$authResult.ExpiresOn
     }
 
+#heres me talking to graph
     $baseurl="https://graph.microsoft.com/beta/deviceAppManagement/"
     $collectionpath="MobileApps"
     $uri="$baseurl$collectionpath"
